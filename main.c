eb9f69c6205472dd62f26944ba67cd26acb42f32
Zhanghaoismine <kegberry@naver.com>
2023-11-09 15:27:23 +0900

    2

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
-	char code1='A';
-	char code2=65;
-	
-	printf("code1=%c, (%d)\n", code1, code1);
-	printf("code2=%c, (%d)\n", code2, code2);
-	system("PAUSE");
-	return 0;
+    int i = 0;
+    char str[4];
+    str[0] = 'a';
+    str[1] = 'b';
+    str[2] = 'c';
+    str[3] = '\0'; // 문자열의 끝을 나타내는 null 문자를 삽입합니다.
+
+    printf("%s\n", str);
+    
+    while (str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    system("PAUSE");
+    return 0;
 }
